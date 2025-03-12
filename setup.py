be996e5d04dd0ba96dc88c8abba2db2668992e36
Sujay Manna <126549102+Sujay309@users.noreply.github.com>
2025-03-12 17:21:21 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='1.0.0',
+    author='sujay_manna',
+    author_email='sujaymanna145@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+    )
